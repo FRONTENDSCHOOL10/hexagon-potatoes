197ed5f756b60df8fb8026151586c5f78ef237c4
채소 <chlthgus0901@naver.com>
2024-09-01 15:39:34 +0900

    feat : shipstate 컴포넌트 구현

---
+++ src/components/shipstate/Shipstate.tsx
@@ -0,0 +1,24 @@
+interface ShipstatPropsType {
+  step: number;
+}
+
+function Shipstate({ step }: ShipstatPropsType) {
+  return (
+    <div className="w-full h-[49px] flex [flex-flow:column] gap-[10px]">
+      <div className="w-full h-[9px] rounded-[31px] bg-[#D9D9D9]">
+        <div
+          className="h-[9px]  rounded-[31px] bg-[#000]"
+          style={{ width: `${step * 25}%` }} // 동적으로 받는 스타일은 테일윈드 백틱이 안먹음
+        ></div>
+      </div>
+      <div className="flex justify-between text-[#000] text-xs not-italic font-normal relative">
+        <span className="absolute left-0">발송완료</span>
+        <span className="absolute left-1/4">통관 중</span>
+        <span className="absolute left-2/4">국내 도착</span>
+        <span className="absolute left-3/4">배송 중</span>
+      </div>
+    </div>
+  );
+}
+
+export default Shipstate;
