68a5e16406e0445e42ce0fb322137d829d519304
채소 <chlthgus0901@naver.com>
2024-09-03 11:04:02 +0900

    feat : 수수료 고정
    - 수수료 1000원 고정 (COMMISSION 상수로 분리함)
    - formatCurrency 타입스크립트 반환 타입 명시
    - tailwind 스타일 수정

--- src/components/Payment/PaymentListItem.tsx
+++ src/components/Payment/PaymentListItem.tsx
@@ -8,7 +8,7 @@ interface PaymentListItemProp {
 function PaymentListItem({ price, children }: PaymentListItemProp) {
   // const formatCurrency = formatCurrency();
   return (
-    <li className=" [border-bottom:1px_solid_#CAD4E7] px-[0] py-[10px] flex justify-between items-start  text-[#000] text-sm not-italic font-light leading-[24px]">
+    <li className=" border-b border-[#CAD4E7] px-[0] py-[10px] flex justify-between items-start  text-[#000] text-sm not-italic font-light leading-[24px]">
       <span>{children}</span>
       <span>{formatCurrency(price)} 원</span>
     </li>
