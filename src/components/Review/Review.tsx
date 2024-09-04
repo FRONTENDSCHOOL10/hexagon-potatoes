91eadf556a3f8f720addf13895bd977ad7859450
채소 <chlthgus0901@naver.com>
2024-09-04 23:10:29 +0900

    feat : 기본 프로필 이미지 추가

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -4,9 +4,18 @@ interface ReviewProp {
   user: string;
   content: string;
   date: string;
-  profileImg?: string;
+  profileImg?: string | null;
   starRate: number;
 }
+
+// profileImg가 없을 경우 기본 프로필 컴포넌트
+const DefaultProfileSVG = (): JSX.Element => {
+  return (
+    <svg role="img" viewBox="0 0 24 24" aria-label="기본 프로필 사진">
+      <use href="/assets/sprite-sheet.svg#defaultprofile" />
+    </svg>
+  );
+};
 
 //리뷰 DB에서 유저 닉네임, 프로필 이미지, 후기내용, 작성날짜, 평점 가져오면될듯
 const Review = ({
@@ -26,7 +35,17 @@ const Review = ({
           aria-label="닉네임"
           className="flex h-[1.5rem] items-center overflow-ellipsis text-center font-[Pretendard] text-xs font-normal not-italic text-[#020715]"
         >
-          <img className="mr-1.5 h-6 w-6" src={profileImg} alt="프로필이미지" />{' '}
+          {profileImg ? (
+            <img
+              className="mr-1.5 h-6 w-6"
+              src={profileImg}
+              alt="프로필이미지"
+            />
+          ) : (
+            <span className="mr-1.5 h-6 w-6">
+              <DefaultProfileSVG />
+            </span>
+          )}{' '}
           {user}
         </span>
         <RatingStar value={starRate} />
@@ -37,7 +56,8 @@ const Review = ({
           WebkitLineClamp: 2,
           WebkitBoxOrient: 'vertical',
         }}
-        className="max-h-[3.75rem] overflow-hidden text-ellipsis font-[Pretendard] text-[0.875rem] font-light not-italic text-[#020715]"
+        className="h-[3.75rem] max-h-[3.75rem] overflow-hidden text-ellipsis font-[Pretendard] text-[0.875rem] font-light not-italic text-[#020715]"
+        aria-label="후기 작성내용"
       >
         {content}
       </div>
