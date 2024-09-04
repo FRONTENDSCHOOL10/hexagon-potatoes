edc3d9d019ad8c0a5c041ad6a32239bf36545137
채소 <chlthgus0901@naver.com>
2024-09-04 22:42:07 +0900

    feat : 사용자후기 컴포넌트  제작
    - Review 컴포넌트 제작
    - RatingStar 컴포넌트 수정

---
+++ src/components/Review/Review.tsx
@@ -0,0 +1,55 @@
+import RatingStar from '@/components/RatingStar/RatingStar';
+
+interface ReviewProp {
+  user: string;
+  content: string;
+  date: string;
+  profileImg?: string;
+  starRate: number;
+}
+
+//리뷰 DB에서 유저 닉네임, 프로필 이미지, 후기내용, 작성날짜, 평점 가져오면될듯
+const Review = ({
+  user,
+  profileImg,
+  content,
+  date,
+  starRate,
+}: ReviewProp): JSX.Element => {
+  return (
+    <article
+      aria-label="사용자후기"
+      className="flex w-[13.75rem] flex-col gap-2 rounded-[0.375rem] bg-[#EDF1F6] p-3"
+    >
+      <header className="flex flex-row items-center justify-between">
+        <span
+          aria-label="닉네임"
+          className="flex h-[1.5rem] items-center overflow-ellipsis text-center font-[Pretendard] text-xs font-normal not-italic text-[#020715]"
+        >
+          <img className="mr-1.5 h-6 w-6" src={profileImg} alt="프로필이미지" />{' '}
+          {user}
+        </span>
+        <RatingStar value={starRate} />
+      </header>
+      <div
+        style={{
+          display: '-webkit-box',
+          WebkitLineClamp: 2,
+          WebkitBoxOrient: 'vertical',
+        }}
+        className="max-h-[3.75rem] overflow-hidden text-ellipsis font-[Pretendard] text-[0.875rem] font-light not-italic text-[#020715]"
+      >
+        {content}
+      </div>
+      <time
+        className="overflow-hidden overflow-ellipsis font-[Pretendard] text-xs font-normal not-italic text-[#626871]"
+        dateTime={date}
+        aria-label="후기 작성시간"
+      >
+        {date}
+      </time>
+    </article>
+  );
+};
+
+export default Review;
