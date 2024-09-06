f7a5fa1fd39265f9f8d64ee1edf29f3299943a43
JAM <99.1.some@gmail.com>
2024-09-06 17:33:28 +0900

    src, modi : 알럿창에 쓸 이미지 추가, type 설정중

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,30 +1,49 @@
 import React, { memo } from 'react';
 
 interface AlertProps {
+  type: 'notice' | 'error';
+  twobutton?: boolean;
   title: string;
   subtext: string;
 }
 
-const Alert: React.FC<AlertProps> = memo(({ title, subtext }) => {
-  return (
-    <div
-      className="relative flex h-[215px] w-[330px] flex-col items-center justify-center gap-3 rounded-[15px] bg-gray-100 p-6"
-      role="alert"
-      aria-live="assertive"
-    >
-      {/* 아이콘 영역 */}
-      <div
-        className="h-[45px] w-[45px] rounded-full bg-white"
-        aria-hidden="true"
-      />
+const renderIcon = (type: string) => {
+  switch (type) {
+    case 'notice':
+      return (
+        <svg className="h-[2.8125rem] w-[2.8125rem]" href="/assets/check.svg" />
+      );
+    case 'error':
+      return (
+        <svg className="w-auto">
+          <use href="/assets/error.svg" />
+        </svg>
+      );
+  }
+};
 
-      {/* 제목과 부제목 영역 */}
-      <div className="flex w-full flex-col items-center gap-1.5 text-center">
-        <h2 className="text-2xl font-medium text-black">{title}</h2>
-        <p className="text-xs font-light text-black">{subtext}</p>
+const Alert: React.FC<AlertProps> = memo(
+  ({ type = 'notice', title, subtext }) => {
+    return (
+      <div
+        className="relative flex h-[215px] w-[330px] flex-col items-center justify-center gap-3 rounded-[15px] bg-gray-100 p-6"
+        role="alert"
+        aria-live="assertive"
+      >
+        {/* 아이콘 영역 */}
+        <div className="h-[45px] w-[45px] rounded-full" aria-hidden="true">
+          {renderIcon(type)}
+        </div>
+
+        {/* 제목과 부제목 영역 */}
+        <div className="flex w-full flex-col gap-1.5 text-center">
+          <h2 className="text-2xl font-medium text-black">{title}</h2>
+          <p className="text-xs font-light text-black">{subtext}</p>
+        </div>
+        <svg className="" href="/assets/check.svg" />
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default Alert;
