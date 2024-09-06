8436dfaaad02a5ae86cbb0daaec0cdb52ed2945e
JAM <99.1.some@gmail.com>
2024-09-07 00:30:21 +0900

    refactor : svg 가져오기 스프라이트 이미지에서 가져오는 걸로 변경

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -12,26 +12,14 @@ const renderIcon = (type: 'notice' | 'error') => {
   switch (type) {
     case 'notice':
       return (
-        <svg fill="none" viewBox="0 0 46 45">
-          <g clipPath="url(#a)">
-            <path
-              fill="#0A73F9"
-              d="M23 0a22.5 22.5 0 1 0 0 45 22.5 22.5 0 0 0 0-45Zm11.6 19.7L26.3 28a7.5 7.5 0 0 1-10.4 0l-3.6-3.5a1.9 1.9 0 1 1 2.6-2.7l3.6 3.5a3.7 3.7 0 0 0 5.2 0L32 17a1.9 1.9 0 1 1 2.6 2.7Z"
-            />
-          </g>
-          <defs>
-            <clipPath id="a">
-              <path fill="#fff" d="M0 0h45v45H0z" transform="translate(.5)" />
-            </clipPath>
-          </defs>
+        <svg className="h-[2.8125rem] w-[2.8125rem]">
+          <use href="/assets/sprite-sheet.svg#check" />
         </svg>
       );
     case 'error':
       return (
-        <svg fill="none" viewBox="0 0 46 45">
-          <circle cx="23" cy="22.5" r="22.5" fill="#E8134B" />
-          <rect width="5" height="21" x="20.5" y="8" fill="#fff" rx="2.5" />
-          <rect width="5" height="5" x="20.5" y="32" fill="#fff" rx="2.5" />
+        <svg className="h-[2.8125rem] w-[2.8125rem]">
+          <use href="/assets/sprite-sheet.svg#error" />
         </svg>
       );
   }
@@ -66,7 +54,7 @@ const Alert: React.FC<AlertProps> = memo(
           isActive={true}
           imgSrc=""
           imgAlt=""
-          onClick={onClose}
+          // onClick={onClose}
         />
       </div>
     );
