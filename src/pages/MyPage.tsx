c0899baf0b99e06ba687499a96a3b0781b800be6
jinjintv <theffffff@naver.com>
2024-09-06 19:23:13 +0900

    modi: 절약금액 닉네임 중복 삭제

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import SavingMoneyCard from '../components/SavingMoneyCard/SavingMoneyCard';
 const MyPage = () => {
+  const nickname = "사용자";
+  // db에서 받아와야함
+
   return (
-    <div>
-      <h1>마이페이지</h1>
+    <div className="grid place-items-center gap-[1.25rem]">
+      {/* <h1>마이페이지</h1> */}
+      <SavingMoneyCard nickname={nickname}/>
     </div>
   );
 };
