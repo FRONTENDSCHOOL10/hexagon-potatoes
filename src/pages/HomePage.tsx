34db27fbe016cbf0ff588042fd22fe397af38b40
JAM <99.1.some@gmail.com>
2024-09-09 01:37:20 +0900

    markup, style : 모든 이미지 일단 로고로 해놓고 마크업, 스타일링

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,138 @@
-import Alert from '@/components/Alert/Alert';
 import React from 'react';
+import { SwiperSlide, Swiper } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination'; // Ensure you import pagination CSS
+
+import SearchBar from '../components/SearchBar/SearchBar';
+import MiniPostingCard from '@/components/PostingCard/MiniPostingCard';
+import SelectCountryButton from '../components/Buttons/SelectCountryButton';
+import PartyLeaderList from '../components/Lists/ParyLeaderList';
+import PartyLeader from '../components/Lists/PartyLeader';
+import PostingCard from '../components/PostingCard/PostingCard';
+import Article from '@/components/Lists/Article';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>홈페이지</h1>
-      <h1>홈페이지</h1>
-      <h1>홈페이지</h1>
-      <h1>홈페이지</h1>
-      <h1>홈페이지</h1>
-      <h1>홈페이지</h1>
-      <h1>홈페이지</h1>
-      <h1>홈페이지</h1>
-      <h1>홈페이지</h1>
-      <h1>홈페이지</h1>
+    <div className="space-y-[12px] px-3">
+      <SearchBar />
+
+      <section>
+        <h1 className="mb-[.75rem] pt-4 font-heading-1 text-heading-1 font-medium">
+          매거진
+        </h1>
+        {/* 매거진 관련 내용 */}
+        <Swiper
+          className="mySwiper"
+          modules={[Navigation]}
+          slidesPerView={3}
+          loop
+          spaceBetween={160}
+          centeredSlides={true}
+          pagination={{ clickable: true }}
+        >
+          <SwiperSlide>
+            <MiniPostingCard nickname={'매거진1'} content={'매거진1'} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <MiniPostingCard nickname={'매거진2'} content={'매거진2'} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <MiniPostingCard nickname={'매거진3'} content={'매거진3'} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <MiniPostingCard nickname={'매거진4'} content={'매거진'} />
+          </SwiperSlide>
+        </Swiper>
+      </section>
+
+      <section>
+        <h1 className="mb-[.75rem] pt-4 font-heading-1 text-heading-1 font-medium">
+          국가 리스트
+        </h1>
+        <div className="flex flex-wrap justify-between gap-[10px_0px]">
+          <SelectCountryButton
+            buttonContent={'미국'}
+            imgAlt={''}
+            pageUrl={''}
+            imgSrc={'shipmatelogo.png'}
+          />
+          <SelectCountryButton
+            buttonContent={'중국'}
+            imgAlt={''}
+            pageUrl={''}
+            imgSrc={'shipmatelogo.png'}
+          />
+          <SelectCountryButton
+            buttonContent={'일본'}
+            imgAlt={''}
+            pageUrl={''}
+            imgSrc={'shipmatelogo.png'}
+          />
+          <SelectCountryButton
+            buttonContent={'그 외 국가'}
+            imgAlt={''}
+            pageUrl={''}
+            imgSrc={'shipmatelogo.png'}
+          />
+        </div>
+      </section>
+
+      <section>
+        {/* 우수 파티장 별점 높은 애들 중 랜덤으로 한명만 보이게 할까 */}
+        <h1 className="mb-[.75rem] pt-4 font-heading-1 text-heading-1 font-medium">
+          이달의 우수 파티장
+        </h1>
+        <PartyLeader
+          gradeImg={'/assets/shipmatelogo.png'}
+          profile_photo={'/assets/shipmatelogo.png'}
+          nickname={'닉네임'}
+          itemImgAlt={''}
+          rating={'3'}
+          member_grade={'골드'}
+          member_description={'소개'}
+          user_id={0}
+        />
+      </section>
+
+      <section>
+        <h1 className="mb-[.75rem] pt-4 font-heading-1 text-heading-1 font-medium">
+          추천 파티 리스트
+        </h1>
+
+        <div>추천 파티 리스트 내용이 여기에 들어갑니다.</div>
+      </section>
+
+      <section>
+        <h1 className="mb-[.75rem] pt-4 font-heading-1 text-heading-1 font-medium">
+          직구 자랑
+        </h1>
+        <div>
+          <PostingCard
+            user={'유저'}
+            content={'얍'}
+            postingImg="/assets/shipmatelogo.png"
+          />
+        </div>
+      </section>
+
+      <section>
+        <h1 className="mb-[.75rem] pt-4 font-heading-1 text-heading-1 font-medium">
+          유저들의 팁
+        </h1>
+        <div>
+          <Article
+            party_name={''}
+            partyImg={''}
+            partyImgAlt={''}
+            party_about={''}
+            profile_photo={''}
+            leaderImgAlt={''}
+            nickname={''}
+          />
+        </div>
+      </section>
     </div>
   );
 };
