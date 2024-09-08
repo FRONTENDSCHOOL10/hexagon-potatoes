70c6c68b5171c116b156a879257cce44dfc51958
채소 <chlthgus0901@naver.com>
2024-09-08 15:26:59 +0900

    feat: MyHaul 컴포넌트 구현 (데이터 조회 및 에러 처리 포함)
    - 조회된 데이터로 InstaPosting 컴포넌트 렌더링
    - 로딩 상태 관리 추가

---
+++ src/pages/Posting/MyHaul.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import InstaPosting from '@/pages/Posting/InstaPosting';
+
+const MyHaul = (): JSX.Element => {
+  const { id } = useParams();
+  const [data, setData] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const ENDPOINT = `https://hexagon-potatoes.pockethost.io/api/collections/posting/records/${id}`;
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    const fetchPosting = async () => {
+      try {
+        const response = await axios.get(ENDPOINT, {
+          signal: abortController.signal,
+          params: {
+            expand: 'author_id',
+          },
+        });
+        console.log(response.data);
+        setData(response.data);
+      } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+          if (error.name !== 'CanceledError') {
+            return;
+          }
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPosting();
+
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
+  if (loading) {
+    // 로딩 스피너 만들면 넣어주기
+    return <div>Loading...</div>;
+  }
+
+  return <>{data ? <InstaPosting item={data} /> : null}</>;
+};
+
+export default MyHaul;
