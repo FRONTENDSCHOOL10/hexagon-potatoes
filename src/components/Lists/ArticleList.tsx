23639ff3b2c65360da5d4f1c969e52b5988f2a29
JubiJung <devvjubi@gmail.com>
2024-09-09 14:29:07 +0900

    modi: ArticleList 컴포넌트 주석 수정

--- src/components/Lists/ArticleList.tsx
+++ src/components/Lists/ArticleList.tsx
@@ -5,7 +5,7 @@ const ArticleList = ({ data }) => {
   // 헤딩 아이디랑 ul을 aria-labelledby 연결하고 싶음.
   return (
     <ul className="flex flex-col gap-y-3">
-      가져온 데이터 전달
+      {/* 가져온 데이터 전달 */}
       {data.map((d) => (
         <Article key={} props={} />
       ))}
