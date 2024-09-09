f12880253459fba680d9706c87167140489bf742
채소 <chlthgus0901@naver.com>
2024-09-09 14:40:57 +0900

    modi : MiniPostingCard 프롭에 photo 추가

--- src/components/PostingCard/MiniPostingCard.tsx
+++ src/components/PostingCard/MiniPostingCard.tsx
@@ -4,15 +4,21 @@ interface MiniPostingCardProp {
   nickname: string;
   content: string;
   id: string;
+  photo: string;
 }
 
-const MiniPostingCard = ({ nickname, content, id }: MiniPostingCardProp) => {
+const MiniPostingCard = ({
+  nickname,
+  content,
+  id,
+  photo,
+}: MiniPostingCardProp) => {
   return (
     <Link to={`/home/instaPosting/${id}`}>
       <figure className="flex-[1_0_0]; flex h-[13.6rem] w-40 flex-col items-center rounded-md bg-[#FFF] shadow-shadow-blue">
         <img
           className="h-[10.3rem] w-[10rem] bg-[rgba(0,_0,_0,_0.05)]"
-          src=""
+          src={photo}
           alt="포스팅 이미지"
         />
         <figcaption className="flex flex-col items-start gap-1 self-stretch p-2">
