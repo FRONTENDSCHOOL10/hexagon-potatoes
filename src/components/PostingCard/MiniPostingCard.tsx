8a2db379ce6e5c9481c77b99ac1a8da9ebb1d2ef
채소 <chlthgus0901@naver.com>
2024-09-10 22:30:31 +0900

    modi : 마크업 관련 오류 수정

--- src/components/PostingCard/MiniPostingCard.tsx
+++ src/components/PostingCard/MiniPostingCard.tsx
@@ -15,15 +15,21 @@ const MiniPostingCard = ({
 }: MiniPostingCardProp) => {
   return (
     <Link to={`/home/instaPosting/${id}`}>
-      <figure className="flex-[1_0_0]; flex h-[13.6rem] w-40 flex-col items-center rounded-md bg-[#FFF] shadow-shadow-blue">
+      <figure
+        aria-labelledby={`post-title-${id}`}
+        className="flex-[1_0_0]; flex h-[13.6rem] w-40 flex-col items-center rounded-md bg-[#FFF] shadow-shadow-blue"
+      >
         <img
           className="h-[10.3rem] w-[10rem] bg-[rgba(0,_0,_0,_0.05)]"
           src={photo}
           alt="포스팅 이미지"
         />
         <figcaption className="flex flex-col items-start gap-1 self-stretch p-2">
-          <caption className="text-caption text-gray-300">{nickname}</caption>
-          <h3 className="text-sub-1 w-full overflow-hidden text-ellipsis whitespace-nowrap text-black">
+          <p className="text-caption text-gray-300">{nickname}</p>
+          <h3
+            id={`post-title-${id}`}
+            className="text-sub-1 w-full overflow-hidden text-ellipsis whitespace-nowrap text-black"
+          >
             {content}
           </h3>
         </figcaption>
