6e70babcc4b445a2aefb91eac42b80907e9541ff
JAM <99.1.some@gmail.com>
2024-09-11 00:52:59 +0900

    modi : 파티리스트 코드 수정 (오류나서 일단 비워둠)

--- src/pages/PartyList.tsx
+++ src/pages/PartyList.tsx
@@ -1,5 +1,3 @@
-import ArticleList from '@/components/Lists/ArticleList';
-
 // 파티 리스트 데이터 받기
 // 그러면 처음부터 국가 선택 페이지에서 현재 선택한 국가 정보를 넘겨줘야할 거 같음
 const partyListData = [{}];
@@ -9,7 +7,6 @@ const PartyListPage = () => {
     <section className="flex flex-col gap-y-3">
       <h1 className="sr-only">파티 리스트 페이지</h1>
       <p className="mt-4 text-heading-1">파티원을 모집 중이에요!</p>
-      <ArticleList />
     </section>
   );
 };
