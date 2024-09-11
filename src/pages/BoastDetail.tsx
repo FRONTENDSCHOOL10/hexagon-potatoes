eb2931b0668c95cf7be304d71aca78a2106da888
채소 <chlthgus0901@naver.com>
2024-09-11 12:14:16 +0900

    refactor: BoastDetail 컴포넌트에 useFetch 커스텀 훅 적용

--- src/pages/BoastDetail.tsx
+++ src/pages/BoastDetail.tsx
@@ -1,45 +1,15 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
 import InstaPosting from '@/pages/Posting/InstaPosting';
+import useFetch from '@/hooks/useFetch';
 
 const BoastDetail = (): JSX.Element => {
   const { boastId } = useParams();
-  const [data, setData] = useState<any>([]);
-  const [loading, setLoading] = useState<boolean>(true);
   const ENDPOINT = `https://hexagon-potatoes.pockethost.io/api/collections/posting/records/${boastId}`;
 
-  useEffect(() => {
-    const abortController = new AbortController();
-
-    const fetchPosting = async () => {
-      try {
-        const response = await axios.get(ENDPOINT, {
-          signal: abortController.signal,
-          params: {
-            expand: 'author_id',
-          },
-        });
-        console.log(response.data);
-        setData(response.data);
-      } catch (error: unknown) {
-        if (axios.isAxiosError(error)) {
-          if (error.name !== 'CanceledError') {
-            return;
-          }
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchPosting();
-
-    return () => {
-      abortController.abort();
-    };
-  }, []);
-
-  if (loading) {
+  const { error, status, data } = useFetch(ENDPOINT, 'author_id');
+  console.log(data);
+  if (status === 'loading') {
     // 로딩 스피너 만들면 넣어주기
     return <div>Loading...</div>;
   }
