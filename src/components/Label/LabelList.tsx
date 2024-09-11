0b3452476f25cc8ed73496837f021dc56916bb89
채소 <chlthgus0901@naver.com>
2024-09-11 23:25:24 +0900

    fix: LabelList 컴포넌트의 데이터 처리 개선
    - 데이터가 없는 경우 처리 로직 추가

--- src/components/Label/LabelList.tsx
+++ src/components/Label/LabelList.tsx
@@ -2,16 +2,14 @@
 import Label from './Label';
 
 interface LabelListProps {
-  data: string[];
+  data?: string[];
 }
 
 const LabelList = ({ data }: LabelListProps) => {
   return (
     <ul className="my-2.5 flex flex-row text-caption">
       {/* 가져온 데이터 전달 */}
-      {data.map((label) => (
-        <Label label={label} key={label} />
-      ))}
+      {data ? data.map((label) => <Label label={label} key={label} />) : ''}
     </ul>
   );
 };
