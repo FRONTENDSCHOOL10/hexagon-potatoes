5b12b3114991679befdc8a5459d3b532eb0f8990
채소 <chlthgus0901@naver.com>
2024-09-11 12:12:13 +0900

    feat: MiniPostingCard 컴포넌트 UI 및 기능 개선
    - 링크 경로 수정
    - 기본 이미지 URL 변수(defaultTipImage) 추가

--- src/components/PostingCard/MiniPostingCard.tsx
+++ src/components/PostingCard/MiniPostingCard.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import defaultTipImageSrc from '/assets/shipmatelogo.png';
 
 interface MiniPostingCardProp {
   nickname: string;
@@ -13,22 +14,25 @@ const MiniPostingCard = ({
   id,
   photo,
 }: MiniPostingCardProp) => {
+  const defaultTipImage = '/assets/shipmatelogo.png'; // 기본 팁 이미지 URL
+
   return (
-    <Link to={`/home/instaPosting/${id}`}>
+    <Link to={`/home/community/boast/${id}`}>
       <figure
         aria-labelledby={`post-title-${id}`}
-        className="flex-[1_0_0]; flex h-[13.6rem] w-40 flex-col items-center rounded-md bg-[#FFF] shadow-shadow-blue"
+        className="flex-[1_0_0]; flex h-[13.6rem] w-40 flex-col items-center overflow-hidden rounded-md bg-[#FFF] shadow-shadow-blue"
       >
         <img
-          className="h-[10.3rem] w-[10rem] bg-[rgba(0,_0,_0,_0.05)]"
-          src={photo}
+          className="h-[10.3rem] w-full bg-[rgba(0,_0,_0,_0.05)] object-cover"
+          src={photo} // null, undefined, 빈 문자열 모두 처리
           alt="포스팅 이미지"
         />
+
         <figcaption className="flex flex-col items-start gap-1 self-stretch p-2">
           <p className="text-caption text-gray-300">{nickname}</p>
           <h3
             id={`post-title-${id}`}
-            className="text-sub-1 w-full overflow-hidden text-ellipsis whitespace-nowrap text-black"
+            className="text-sub-1 w-full overflow-hidden text-ellipsis whitespace-nowrap leading-[1.18rem] text-black"
           >
             {content}
           </h3>
