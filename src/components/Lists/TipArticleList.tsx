a5cc4f11b05b2a7c8c87296a03d5f52982d8e6e0
채소 <chlthgus0901@naver.com>
2024-09-11 12:10:30 +0900

    feat : TipArticleList 컴포넌트 기능 개선 및 데이터 처리 최적화
    - key값을 index에서 item.id로 변경
    - slice() 메서드로 최대 3개의 팁만 표시 제한
    - 확장된 작성자 데이터에서 닉네임 정보 가져오도록 수정

--- src/components/Lists/TipArticleList.tsx
+++ src/components/Lists/TipArticleList.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
 import Article from './Article';
 import DefaultProfileSVG from '../DefaultProfileSVG/DefaultProfileSVG';
+import getPbImageURL from '@/utils/getPbImageURL';
 
 interface TipArticleListProps {
   data: {
     type: 'tip'; // 항상 'tip'으로 설정
     title: string; // 팁 제목
-    photo: string; // 팁 이미지 URL
+    photo: string | null; // 팁 이미지 URL
     content: string; // 팁 내용
     author_photo: string; // 작성자의 프로필 사진 URL
     author_nickname: string; // 작성자의 닉네임
     id: string; // 팁의 식별자
+    collectionId: string;
+    expand?: {
+      // 'expand' 속성 추가
+      author_id?: {
+        profile_photo: string | null;
+      };
+    };
   }[];
 }
 
@@ -31,23 +38,27 @@ const TipArticleList = ({ data }: TipArticleListProps) => {
    * @param item - 팁 데이터 항목.
    * @returns 처리된 팁 데이터 항목.
    */
-  const processedData = data.map((item) => ({
-    ...item,
-    photo: item.photo || defaultTipImage, // 팁 이미지가 없으면 기본 이미지 사용
-    author_photo: item.author_photo || defaultProfileImage, // 작성자 프로필 이미지가 없으면 기본 이미지 사용
-  }));
 
+  const tipUrl = `https://hexagon-potatoes.pockethost.io`;
+
+  // console.log(data);
   return (
     <ul className="flex w-full flex-col gap-y-3" aria-label="팁 목록">
-      {processedData.map((item, index) => (
+      {data?.slice(0, 3).map((item, index) => (
         <Article
-          key={index} // 리스트 항목에 대한 고유 키
-          type={item.type} // 'tip'으로 고정
+          key={item.id} // 리스트 항목에 대한 고유 키
+          type={'tip'} // 'tip'으로 고정
           content_title={item.title} // 팁 제목
-          content_img={item.photo} // 팁 이미지
+          content_img={
+            item.photo ? getPbImageURL(tipUrl, item) : defaultTipImage
+          } // 팁 이미지
           subtitle={item.content} // 팁 내용
-          profile_photo={item.author_photo} // 작성자 프로필 사진
-          nickname={item.author_nickname} // 작성자 닉네임
+          profile_photo={
+            item?.expand?.author_id?.profile_photo
+              ? getPbImageURL(tipUrl, item.expand?.author_id, 'profile_photo')
+              : defaultProfileImage
+          } // 작성자 프로필 사진
+          nickname={item.expand?.author_id.nickname} // 작성자 닉네임
           id={item.id} // 팁 식별자
         />
       ))}
