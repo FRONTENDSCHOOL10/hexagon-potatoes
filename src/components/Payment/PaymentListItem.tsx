6824d7d1d1d906fc91e6b5dded183cbfc74cde2e
채소 <chlthgus0901@naver.com>
2024-09-13 17:15:25 +0900

    refactor : 태그 수정
    - li 태그에서 dl 태그로 수정

--- src/components/Payment/PaymentListItem.tsx
+++ src/components/Payment/PaymentListItem.tsx
@@ -11,10 +11,10 @@ const PaymentListItem = ({
 }: PaymentListItemProp): JSX.Element => {
   // const formatCurrency = formatCurrency();
   return (
-    <li className="flex items-start justify-between border-b border-[#CAD4E7] px-[0] py-[0.625rem] text-sm font-light not-italic leading-[1.5rem] text-[#000]">
-      <span>{children}</span>
-      <span>{formatCurrency(price)} 원</span>
-    </li>
+    <div className="flex items-start justify-between border-b border-[#CAD4E7] px-[0] py-[0.625rem] text-sm font-light not-italic leading-[1.5rem] text-[#000]">
+      <dt>{children}</dt>
+      <dd>{formatCurrency(price)} 원</dd>
+    </div>
   );
 };
 
