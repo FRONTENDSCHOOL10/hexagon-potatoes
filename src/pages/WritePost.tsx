be743e832b73c0a5ff373d2f66772364c953277a
채소 <chlthgus0901@naver.com>
2024-09-16 17:35:39 +0900

    feat : WritePost 페이지 컴포넌트 구현
    - 카테고리 선택 기능 추가 ('자랑'/'유저팁')
    - 카테고리별 맞춤 폼 렌더링 구현
    - '유저팁' 카테고리에 제목 입력 필드 추가

--- src/pages/WritePost.tsx
+++ src/pages/WritePost.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import Dropdown from '@/components/Dropdown/Dropdown';
+import FileInput from '@/components/FileInput';
+import HashtagInput from '@/components/HashtagInput/HashtagInput';
+import { useState } from 'react';
+
+const TipPostingForm = () => (
+  <div className="flex flex-col gap-3">
+    <FileInput />
+    <HashtagInput />
+    <input
+      type="text"
+      placeholder="제목을 입력해주세요"
+      className="pretendard h-[2.8125rem] w-full rounded-xl border border-solid border-gray-200 px-5 py-2 text-[0.875rem] font-normal not-italic leading-5 placeholder:text-gray-200"
+    />
+    <textarea
+      placeholder="본문을 입력하세요"
+      className="mb-3 h-[25rem] resize-none overflow-hidden overflow-ellipsis whitespace-nowrap rounded-xl border border-gray-200 p-5 font-[Pretendard] text-[0.875rem] font-normal not-italic leading-5 placeholder:text-gray-200"
+    ></textarea>
+  </div>
+);
+const BoastPostingForm = () => (
+  <div className="flex flex-col gap-3">
+    <FileInput />
+    <HashtagInput />
+    <textarea
+      placeholder="본문을 입력하세요"
+      className="mb-3 h-[25rem] resize-none overflow-hidden overflow-ellipsis whitespace-nowrap rounded-xl border border-gray-200 p-5 font-[Pretendard] text-[0.875rem] font-normal not-italic leading-5 placeholder:text-gray-200"
+    ></textarea>
+  </div>
+);
 
 const WritePost = () => {
+  const [selectedCategory, setSelectedCategory] = useState('자랑');
+  const handleChange = (name: string) => (value: string) => {
+    if (name === '카테고리') {
+      setSelectedCategory(value);
+    }
+  };
   return (
-    <div>
-      <h1>포스트작성</h1>
+    <div className="flex flex-col gap-3 p-3">
+      <Dropdown
+        dropdownName={'카테고리'}
+        list={['유저팁']}
+        label={'카테고리'}
+        defaultMsg={'자랑'}
+        onInputChange={handleChange}
+      />
+      {selectedCategory === '유저팁' ? (
+        <TipPostingForm />
+      ) : (
+        <BoastPostingForm />
+      )}
     </div>
   );
 };
-
 export default WritePost;
