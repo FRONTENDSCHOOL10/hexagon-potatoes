36bedcb6edfd0a6ca1910597c90f3fc826a90523
채소 <chlthgus0901@naver.com>
2024-09-18 00:15:13 +0900

    modi : userTip 페이지 팁 데이터 연결

--- src/pages/Community/UserTip.tsx
+++ src/pages/Community/UserTip.tsx
@@ -1,10 +1,22 @@
+import useFetch from '@/hooks/useFetch';
+import BlogPosting from '../Posting/BlogPosting';
+
 const UserTip = () => {
-  return (
-    <div>
-      여기는 유저팁 입니다아 아마도 블로그 형식으로 쭈욱 이어져야하는데 블로그
-      형식이 뭐저?
-    </div>
-  );
+  const ENDPOINT = `https://hexagon-potatoes.pockethost.io/api/collections/tip/records`;
+
+  const { error, status, data } = useFetch(ENDPOINT, 'author_id');
+  const tipData = data?.items;
+  console.log(tipData);
+
+  if (status === 'loading') {
+    // 로딩 스피너 만들면 넣어주기
+    return <div>Loading...</div>;
+  }
+  if (!tipData || tipData.length === 0) {
+    return <div>No data available</div>;
+  }
+
+  return <>{tipData?.map((d: any) => <BlogPosting key={d.id} item={d} />)}</>;
 };
 
 export default UserTip;
