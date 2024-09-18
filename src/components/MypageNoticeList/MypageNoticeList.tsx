84915c4dc878b4a0584a1720cff66dda868d9934
jinjintv <theffffff@naver.com>
2024-09-19 07:05:22 +0900

    refactor: notice list 버튼에 따라 바뀌게 수정

--- src/components/MypageNoticeList/MypageNoticeList.tsx
+++ src/components/MypageNoticeList/MypageNoticeList.tsx
@@ -1,53 +1,18 @@
-const MypageNoticeList = ({}) => {
-    return (
-      <div className="p-4 space-y-8 w-[22.5rem]">
-        <div>
-          <h2 className="text-[1rem] font-bold mb-2">양떼들의 소식</h2>
-          <ul className="space-y-2 text-[0.875rem]">
-            <li>
-              <a href="#" className="hover:text-mainblue cursor-pointer">이벤트</a>
+const MypageNoticeList = ({ title, items }) => {
+  return (
+    <div className="p-4 space-y-8 w-[22.5rem]">
+      <div>
+        <h2 className="text-[1rem] font-bold mb-2">{title}</h2>
+        <ul className="space-y-2 text-[0.875rem]">
+          {items.map((item, index) => (
+            <li key={index}>
+              <a href="#" className="hover:text-mainblue cursor-pointer">{item}</a>
             </li>
-            <li>
-              <a href="#" className="hover:text-mainblue cursor-pointer">공지사항</a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h2 className="text-[1rem] font-bold mb-2">나의 파티</h2>
-          <ul className="space-y-2 text-[0.875rem]">
-            <li>
-              <a href="#" className="hover:text-mainblue cursor-pointer">개설 파티 목록</a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-mainblue cursor-pointer">개설 파티 후기</a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h2 className="text-[1rem] font-bold mb-2">기타</h2>
-          <ul className="space-y-2 text-[0.875rem]">
-            <li>
-              <a href="#" className="hover:text-mainblue cursor-pointer">내 계좌 관리</a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-mainblue cursor-pointer">키워드 알림 설정</a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h2 className="text-[1rem] font-bold mb-2">버전</h2>
-          <ul className="space-y-2 text-[0.875rem]">
-            <li>
-              <a href="#" className="hover:text-mainblue cursor-pointer">자주 묻는 질문</a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-mainblue cursor-pointer">약관 및 정책</a>
-            </li>
-          </ul>
-        </div>
+          ))}
+        </ul>
       </div>
-    );
-  };
-  
-  export default MypageNoticeList;
-  
+    </div>
+  );
+};
+
+export default MypageNoticeList;
