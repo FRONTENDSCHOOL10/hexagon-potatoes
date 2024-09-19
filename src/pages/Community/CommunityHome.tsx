2e78bbda36124a7077d121c19fc52d9336d04cfa
채소 <chlthgus0901@naver.com>
2024-09-19 13:57:35 +0900

    feat: 커뮤니티 홈 페이지 팁, 자랑게시물 랜덤하게 랜더링
    - getRandomItems 유틸 함수 구현하여 배열에서 랜덤 항목 선택

--- src/pages/Community/CommunityHome.tsx
+++ src/pages/Community/CommunityHome.tsx
@@ -1,12 +1,11 @@
 import TipArticleList from '@/components/Lists/TipArticleList';
 import MiniPostingCard from '@/components/PostingCard/MiniPostingCard';
 import useFetch from '@/hooks/useFetch';
-import getPbImageURL from '@/utils/getPbImageURL';
+import getRandomItems from '@/utils/getRandomItems';
 
 const CommunityHome = () => {
-  const ENDPOINT = `https://hexagon-potatoes.pockethost.io`;
-  const tipUrl = `${ENDPOINT}/api/collections/tip/records`;
-  const boastUrl = `${ENDPOINT}/api/collections/posting/records`;
+  const tipUrl = `${import.meta.env.VITE_PB_URL}/api/collections/tip/records`;
+  const boastUrl = `${import.meta.env.VITE_PB_URL}/api/collections/posting/records`;
 
   const {
     status: tipStatus,
@@ -18,35 +17,35 @@ const CommunityHome = () => {
     error: boastError,
     data: boastData,
   } = useFetch(boastUrl, 'author_id');
-  // console.log(tipData?.items);
 
-  const tipDatas = tipData?.items;
-  const boastDatas = boastData?.items;
-  // console.log(boastDatas);
+  const tipDatas = tipData?.items ?? [];
+  const randomTips = getRandomItems(tipDatas, 3);
+
+  const boastDatas = boastData?.items ?? [];
+  const randomBoasts = getRandomItems(boastDatas, 2);
+
   return (
     <>
       <section className="mb-3 flex w-[22.5rem] flex-col items-center justify-center gap-2 self-stretch px-3 py-[0rem]">
         <h2 className="self-stretch pt-3 text-heading-1 text-[black]">
           유저들의 팁
         </h2>
-        <TipArticleList data={tipDatas} />
+        <TipArticleList data={randomTips} />
       </section>
       <section className="flex w-[22.5rem] flex-col items-center justify-center gap-2 p-3">
         <h2 className="self-stretch pt-3 text-heading-1 text-[black]">
           유저들의 자랑
         </h2>
         <div className="flex h-[14.8125rem] w-[22.5rem] items-center justify-center gap-3">
-          {boastDatas
-            ?.slice(0, 2)
-            .map((d: any) => (
-              <MiniPostingCard
-                key={d?.id}
-                id={d?.id}
-                nickname={d?.expand?.author_id?.nickname}
-                content={d?.content}
-                photo={d.photo.length !== 0 ? d : null}
-              />
-            ))}
+          {randomBoasts.map((d: any) => (
+            <MiniPostingCard
+              key={d?.id}
+              id={d?.id}
+              nickname={d?.expand?.author_id?.nickname}
+              content={d?.content}
+              photo={d.photo.length !== 0 ? d : null}
+            />
+          ))}
         </div>
       </section>
     </>
