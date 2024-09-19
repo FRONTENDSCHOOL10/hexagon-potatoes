bbb8af7e8c0df3cfe2a6b6a25a0eb3beda6d0564
채소 <chlthgus0901@naver.com>
2024-09-19 14:41:52 +0900

    refactor : 드롭박스 화살표 레이아웃 수정
    - 기존 select 태그 스타일 apparance none처리하고 background에 angle svg 추가

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -16,6 +16,21 @@ const Dropdown = ({
   onInputChange,
 }: PropTypes) => {
   const dropdownId = useId();
+  // 아이콘을 위한 스타일
+  const selectStyle = {
+    backgroundImage: `url("data:image/svg+xml;charset=utf-8,${encodeURIComponent(
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">' +
+        '<g transform="rotate(270 12 12)">' +
+        '<path fill="%23000000" d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>' +
+        '</g></svg>'
+    )}")`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'right 10px center',
+    backgroundSize: '20px 20px',
+    paddingRight: '40px',
+    paddingLeft: '20px',
+    textAlign: 'left',
+  };
 
   const handleDropdownVal = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const inputVal = e.target.value;
@@ -29,12 +44,15 @@ const Dropdown = ({
       </label>
 
       <select
-        className="w-full rounded-xl border border-gray-200 py-2 pl-5 pr-5 text-sub-2 outline-1 outline-mainblue"
+        className="w-full appearance-none rounded-xl border border-gray-200 py-2 pl-5 text-sub-2 outline-1 outline-mainblue"
+        style={selectStyle}
         onChange={handleDropdownVal}
         name={dropdownName}
         id={dropdownId}
       >
-        <option value="choice">{defaultMsg}</option>
+        <option value="choice" className="">
+          {defaultMsg}
+        </option>
         {list.map((data) =>
           typeof data === 'string' ? (
             <option key={data} value={data}>
