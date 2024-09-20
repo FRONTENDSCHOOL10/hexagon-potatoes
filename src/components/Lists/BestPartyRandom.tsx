b3272800ad31a2b67df1963f042a46d6c31df75e
채소 <chlthgus0901@naver.com>
2024-09-20 12:02:01 +0900

    refactor : 현재 진행중인 파티 안뜨는 부분 수정

--- src/components/Lists/BestPartyRandom.tsx
+++ src/components/Lists/BestPartyRandom.tsx
@@ -6,7 +6,7 @@ import PartyLeader from './PartyLeader';
 import getPbImageURL from '@/utils/getPbImageURL';
 
 const partyBaseUrl = `${pb.baseUrl}api/collections/party/records`;
-const url = 'https://hexagon-potatoes.pockethost.io';
+const url = `${pb.baseUrl}`;
 
 interface User {
   id: string;
@@ -29,7 +29,12 @@ interface Party {
   };
 }
 
-interface BestPartyRandomProps {
+interface RandomBestLeader {
+  leaderData: User;
+  partyData: Party[];
+}
+
+interface PropTypes {
   type?: 'party' | 'user';
 }
 
@@ -39,9 +44,10 @@ interface BestPartyRandomProps {
 // 로직이 비슷해 보이는 BestPartyLeaderRandom을 사용하는 대신
 // BestPartyRandom 한컴포넌트 안에서 type만 다르게 줘서 처리하게 했습니다
 
-const BestPartyRandom: React.FC<BestPartyRandomProps> = ({ type = 'user' }) => {
+const BestPartyRandom = ({ type = 'user' }: PropTypes) => {
   const [randomParty, setRandomParty] = useState<Party | null>(null);
-  const [randomBestLeader, setRandomBestLeader] = useState<User | null>(null);
+  const [randomBestLeader, setRandomBestLeader] =
+    useState<RandomBestLeader | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -56,7 +62,7 @@ const BestPartyRandom: React.FC<BestPartyRandomProps> = ({ type = 'user' }) => {
       // 파티 콜렉션에서 expand로 party_leader를 하고 filter로 "party_leader.rating>=4" 를 해서 한번의 데이터 요청으로 줄임
       // 결론 : 파티 리더의 별점이 4이상인 리더의 파티를 요청
       try {
-        const response = await axios.get(partyBaseUrl, {
+        const response = await axios.get<{ items: Party[] }>(partyBaseUrl, {
           params: {
             filter: 'party_leader.rating>=4',
             expand: 'party_leader',
@@ -69,16 +75,26 @@ const BestPartyRandom: React.FC<BestPartyRandomProps> = ({ type = 'user' }) => {
         if (parties.length === 0) {
           throw new Error('우수 평점을 가진 파티가 없습니다.');
         }
-
         // 랜덤한 숫자를 만듦
         // 한 컴포넌트에서 처리하기 때문에 파티 랜덤넘버, 리더 랜덤넘버 구분
         const partyNum = Math.floor(Math.random() * parties.length);
         const leaderNum = Math.floor(Math.random() * parties.length);
+        // 랜덤으로 구한 "parties[leaderNum].party_leader" 파티리더를 평점 4점이상 파티의 리더와 비교해서
+        // 일치하는것들만 배열로 만듦 => PartyLeader 컴포넌트에 리더 정보뿐만이아니라 파티 정보까지 필요해서..
+        const newArray = parties.filter(
+          (party) => party.party_leader === parties[leaderNum].party_leader
+        );
 
-        // 조회한 파티 데이터에 랜덤한 숫자로 랜덤파티 상태 업데이트
-        setRandomParty(parties[partyNum]);
-        // 조회한 파티 데이터에 랜덤한 숫자로 랜덤파티 가져와서 expand한 파티리더 정보로 랜덤베스트리더 상태 업데이트
-        setRandomBestLeader(parties[leaderNum].expand?.party_leader || null);
+        if (type === 'party') {
+          // 조회한 파티 데이터에 랜덤한 숫자로 랜덤파티 상태 업데이트
+          setRandomParty(parties[partyNum]);
+        } else {
+          // 조회한 파티 데이터에 랜덤한 숫자로 랜덤파티 가져와서 expand한 파티리더 정보와 위에서 필터링한 파티데이터 배열 랜덤베스트리더 상태 업데이트
+          setRandomBestLeader({
+            leaderData: parties[leaderNum].expand!.party_leader,
+            partyData: newArray,
+          });
+        }
 
         setLoading(false);
       } catch (err) {
@@ -97,12 +113,12 @@ const BestPartyRandom: React.FC<BestPartyRandomProps> = ({ type = 'user' }) => {
     return () => {
       controller.abort();
     };
-  }, []);
+  }, [type]);
 
   if (loading) return <p>로딩 중...</p>;
   if (error) return <p>{error}</p>;
 
-  const countryImg = (country: string) => {
+  const countryImg = (country: string): string => {
     switch (country) {
       case '미국':
         return '/assets/country/american-flag.webp';
@@ -139,14 +155,18 @@ const BestPartyRandom: React.FC<BestPartyRandomProps> = ({ type = 'user' }) => {
     <div>
       {randomBestLeader ? (
         <PartyLeader
-          key={randomBestLeader.id}
-          profile_photo={getPbImageURL(url, randomBestLeader, 'profile_photo')}
-          nickname={randomBestLeader.nickname}
-          rating={randomBestLeader.rating}
-          user_id={randomBestLeader.id}
-          gradeImg={randomBestLeader.gradeImg}
-          party_number={randomBestLeader.partyCount}
-          member_description={randomBestLeader.user_desc}
+          key={randomBestLeader.leaderData.id}
+          profile_photo={getPbImageURL(
+            url,
+            randomBestLeader.leaderData,
+            'profile_photo'
+          )}
+          nickname={randomBestLeader.leaderData.nickname}
+          rating={randomBestLeader.leaderData.rating}
+          user_id={randomBestLeader.leaderData.id}
+          gradeImg={randomBestLeader.leaderData.gradeImg}
+          party_number={randomBestLeader.partyData.length}
+          member_description={randomBestLeader.leaderData.user_desc}
         />
       ) : (
         <p>우수 파티장 정보를 불러오는 데 실패했습니다.</p>
